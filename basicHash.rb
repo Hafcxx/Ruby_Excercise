0113d6407c3b67fdae7d27d3f2d7a546b0dbdef6
Claudio Aninir <claudio.aninir01@gmail.com>
2022-11-18 19:00:00 -0300

    hasheado de contraseñas listo

---
+++ basicHash.rb
@@ -0,0 +1,17 @@
+def hash (a)
+    arrAlpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789@#".split('')
+    hashed = ""
+    a.split('').each do |b|
+        arrAlpha.each_with_index do |c, i|
+             if b == c 
+                hashed+="#{i}"
+             end
+        end
+    end 
+    return hashed
+end
+
+puts "Introduce una contraseña"
+a = gets.chomp
+
+puts "tu contraseña hasheada es #{hash(a)}"
